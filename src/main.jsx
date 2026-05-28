df0e66de277ef998b7430e28228da1e664ecea45
Golf Outing <golf@outing.app>
2026-05-28 02:08:13 +0000

    Fix ConfirmModal: mount provider in main.jsx not App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './contexts/AuthContext'
+import { ConfirmModalProvider } from './components/ConfirmModal'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ConfirmModalProvider>
+          <App />
+        </ConfirmModalProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
